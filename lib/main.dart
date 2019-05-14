c056de8e87e4cb126662e1e937e957b91ed6ac05
Antedesk <antedesk@gmail.com>
2019-05-15 01:52:53 +0200

    added more example about Image widget utilization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+// Constant variables for the definition of the images' path
+const _PATH = "assets/images";
+const _PIC01 = "$_PATH/sunset.jpg";
+const _PIC02 = "$_PATH/dog1.jpg";
+const _PIC03 = "$_PATH/dog2.jpg";
+const _PIC04 = "$_PATH/fox.jpg";
+const _PIC05 = "$_PATH/sping.jpg";
 
 void main() => runApp(MyApp());
 
@@ -9,103 +18,151 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Lesson 12 - Assets and Images'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          Container(
+            constraints: BoxConstraints.expand(height: 300),
+            alignment: Alignment.center,
+            child: Image.asset(
+              _PIC02,
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          Text("width = 200, height = 200"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC01,
+              height: 200,
+              width: 200,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          Text("Boxfit=fill, width = 150, height = 150"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.fill,
+              height: 150,
+              width: 150,
+            ),
+          ),
+          Text("Boxfit=fitWidth, width = 150, height = 150"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.fitWidth,
+              height: 150,
+              width: 150,
+            ),
+          ),
+          Text("Boxfit=fitHeight, width = 150, height = 150"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.fitHeight,
+              height: 150,
+              width: 150,
+            ),
+          ),
+          Text("Boxfit=cover, width = 150, height = 150"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.cover,
+              height: 150,
+              width: 150,
+            ),
+          ),
+          Text("Boxfit=cover, width = 150, height = 150"),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            width: 150,
+            height: 150,
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+          Text("Boxfit=none, width = 150, height = 150"),
+          SizedBox(width: 100),
+          Container(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.only(bottom: 15),
+            width: 150,
+            height: 150,
+            child: Image.asset(
+              _PIC01,
+              fit: BoxFit.none,
+            ),
+          ),
+          Text("repeatX"),
+          Container(
+            constraints: BoxConstraints.expand(height: 50),
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC03,
+              repeat: ImageRepeat.repeatX,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Text("color and colorBlendMode"),
+          Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.asset(
+              _PIC04,
+              color: Colors.purple,
+              colorBlendMode: BlendMode.colorDodge,
+            ),
+          ),
+          Text("Image loaded from web"),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.network(
+                'https://www.html.it/app/themes/html-2018/dist/images/logo-open.png'),
+          ),
+          Text("Image loaded from web and saved in cache"),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.only(bottom: 15),
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://www.html.it/app/themes/html-2018/dist/images/logo-open.png',
+              placeholder: (context, url) => new CircularProgressIndicator(),
+              errorWidget: (context, url, error) => new Icon(
+                    Icons.error,
+                    color: Colors.red,
+                  ),
+              fadeInDuration: Duration(seconds: 3),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
