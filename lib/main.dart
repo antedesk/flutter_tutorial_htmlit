1861664937555e7ab3a81b016dd6cd110e6af60c
Antedesk <antedesk@gmail.com>
2019-06-21 01:37:52 +0200

    added examples for lesson 15

--- lib/main.dart
+++ lib/main.dart
@@ -7,105 +7,183 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Lesson 15 - RaisedButton and SimpleDialog'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked"); // print in console
+            },
+            child: Text(
+              "Click Me",
+              style: TextStyle(color: Colors.teal, fontSize: 30),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("click");
+            },
+            color: Colors.red,
+            child: Icon(Icons.add),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked"); // print in console
+            },
+            onHighlightChanged: (isHigh) {
+              print("onHighlightChanged.isHigh = $isHigh");
+            },
+            child: Text(
+              "Button with onHightlightChanged",
+              style: TextStyle(color: Colors.teal, fontSize: 25),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            child: Text(
+              "Button with colored text, highlight and splash",
+              style: TextStyle(fontSize: 25),
+            ),
+            color: Colors.purple,
+            textColor: Colors.white,
+            highlightColor: Colors.red,
+            splashColor: Colors.black26
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Text(
+              "Disabled button",
+              style: TextStyle(fontSize: 25),
+            ),
+            disabledColor: Colors.yellow,
+            disabledTextColor: Colors.black,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            child: Text(
+              "Button with textTheme:normal",
+              style: TextStyle(fontSize: 20),
+            ),
+            textTheme: ButtonTextTheme.normal
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            child: Text(
+              "Button with textTheme:accent",
+              style: TextStyle(fontSize: 20),
+            ),
+            textTheme: ButtonTextTheme.accent
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            child: Text(
+              "Button with textTheme:primary",
+              style: TextStyle(fontSize: 20),
+            ),
+            textTheme: ButtonTextTheme.primary
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            child: Text(
+              "Button with textColor, colorBrightness",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            color: Colors.lightBlue,
+            textColor: Colors.white,
+            colorBrightness: Brightness.light,
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text(
+              "Button with elevation and highlightElevation",
+              style: TextStyle(fontSize: 20),
+            ),
+            color: Colors.teal[700],
+            textColor: Colors.white,
+            elevation: 5,
+            highlightElevation: 10,
+            highlightColor: Colors.lightGreen,
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Text(
+              "Button with disabledElevation, Text and Color",
+              style: TextStyle(fontSize: 20),
+            ),
+            color: Colors.red[900],
+            disabledElevation: 10,
+            disabledTextColor: Colors.black,
+            disabledColor: Colors.yellow,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            color: Colors.green,
+            child: Text(
+              "Button with padding",
+              style: TextStyle(fontSize: 20),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            color: Colors.teal[700],
+            child: Text(
+              "Button with StadiumBorder",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+            shape: StadiumBorder(
+              side: BorderSide(color: Colors.lightGreen, width: 1),
+            ),
+          ),
+          SizedBox(height: 10),
+          RaisedButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            color: Colors.lightGreenAccent,
+            child: Text(
+              "Button with rounded rectangle border",
+              style: TextStyle(fontSize: 20),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black26, width: 4),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+        ],
+      )),
     );
   }
 }
