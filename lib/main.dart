d863a6827f7427fabd1a4224f72787bb1795b571
Antedesk <antedesk@gmail.com>
2019-07-08 00:03:51 +0200

    added examples about Row and Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,105 +7,365 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+class MyPage extends StatelessWidget {
+  Widget _rowMainAxisAlignment(mainAxisAlignment) => Container(
+        color: Colors.lightBlue[50],
+        child: Row(
+          mainAxisAlignment: mainAxisAlignment,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              "Text1",
+              style: TextStyle(color: Colors.black, fontSize: 17),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              "Text2",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text3",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text4",
+              style: TextStyle(color: Colors.black, fontSize: 17),
             ),
           ],
         ),
+        height: 30,
+        margin: EdgeInsets.all(5),
+      );
+
+  Widget _columnMainAxisAlignment(mainAxisAlignment) => Container(
+        color: Colors.lightBlue[50],
+        child: Column(
+          mainAxisAlignment: mainAxisAlignment,
+          children: <Widget>[
+            Text(
+              "Text1",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text2",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text3",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text4",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+          ],
+        ),
+        height: 150,
+        margin: EdgeInsets.all(5),
+      );
+
+
+  Widget _rowCrossAxisAlignment(crossAxisAlignment) => Container(
+    color: Colors.lightGreen[50],
+    child: Row(
+      crossAxisAlignment: crossAxisAlignment,
+      children: <Widget>[
+        Text(
+          "Text1",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text2",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text3",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text4",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+      ],
+    ),
+    height: 50,
+    margin: EdgeInsets.all(5),
+  );
+
+  Widget _columnCrossAxisAlignment(crossAxisAlignment) => Container(
+    color: Colors.lightGreen[50],
+    child: Column(
+      crossAxisAlignment: crossAxisAlignment,
+      children: <Widget>[
+        Text(
+          "Text1",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text2",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text3",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        Text(
+          "Text4",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+      ],
+    ),
+    height: 80,
+    width: 200,
+    margin: EdgeInsets.all(5),
+  );
+
+  Widget _rowMainAxisSize(mainAxisSize) => Container(
+      color:
+          mainAxisSize == MainAxisSize.min ? Colors.pink : Colors.yellowAccent,
+      child: Row(
+        mainAxisSize: mainAxisSize,
+        children: <Widget>[
+          Text(
+            "Text1",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+          Text(
+            "Text2",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+          Text(
+            "Text3",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+          Text(
+            "Text4",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+        ],
+      ));
+
+  Widget _columnVerticalDirection(verticalDirection) => Container(
+        color: Colors.lightBlue[50],
+        child: Column(
+          verticalDirection: verticalDirection,
+          children: <Widget>[
+            Text(
+              "Text1",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text2",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text3",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            Text(
+              "Text4",
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+          ],
+        ),
+        height: 150,
+        margin: EdgeInsets.all(5),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lesson 16'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          // Rows MainAxisAlignment
+          Text(
+            "Basic Row",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+              color: Colors.orange[100],
+              child: Row(children: <Widget>[
+                Text(
+                  "Text1",
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                ),
+                Text(
+                  "Text2",
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                )
+              ])),
+          SizedBox(height: 10),
+          Text(
+            "Basic Column",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+              color: Colors.orange[100],
+              child: Column(children: <Widget>[
+                Text(
+                  "Text1",
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                ),
+                Text(
+                  "Text2",
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                )
+              ])),
+          SizedBox(height: 10),
+
+          // Rows MainAxisAlignment
+          Text(
+            "Row MainAxisAlignment.start",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.start),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisAlignment.center",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.center),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisAlignment.end",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.end),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisAlignment.spaceBetween",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.spaceBetween),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisAlignment.spaceEvenly",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.spaceEvenly),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisAlignment.spaceAround",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisAlignment(MainAxisAlignment.spaceAround),
+
+          // Column MainAxisAlignment
+          Text(
+            "Column MainAxisAlignment.start",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.start),
+          SizedBox(height: 10),
+          Text(
+            "Column MainAxisAlignment.center",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.center),
+          SizedBox(height: 10),
+          Text(
+            "Column MainAxisAlignment.end",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.end),
+          SizedBox(height: 10),
+          Text(
+            "Column MainAxisAlignment.spaceBetween",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.spaceBetween),
+          SizedBox(height: 10),
+          Text(
+            "Column MainAxisAlignment.spaceEvenly",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.spaceEvenly),
+          SizedBox(height: 10),
+          Text(
+            "Column MainAxisAlignment.spaceAround",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnMainAxisAlignment(MainAxisAlignment.spaceAround),
+
+          // Row CrossAxisAlignment
+          SizedBox(height: 10),
+          Text(
+            "Row CrossAxisAlignment.start",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowCrossAxisAlignment(CrossAxisAlignment.start),
+          SizedBox(height: 10),
+          Text(
+            "Row CrossAxisAlignment.center",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowCrossAxisAlignment(CrossAxisAlignment.center),
+          SizedBox(height: 10),
+          Text(
+            "Row CrossAxisAlignment.end",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowCrossAxisAlignment(CrossAxisAlignment.end),
+          // Column CrossAxisAlignment
+          SizedBox(height: 10),
+          Text(
+            "Column CrossAxisAlignment.start",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnCrossAxisAlignment(CrossAxisAlignment.start),
+          SizedBox(height: 10),
+          Text(
+            "Column CrossAxisAlignment.center",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnCrossAxisAlignment(CrossAxisAlignment.center),
+          SizedBox(height: 10),
+          Text(
+            "Column CrossAxisAlignment.end",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnCrossAxisAlignment(CrossAxisAlignment.end),
+
+          // MainAxisSize
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisSize.min",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+
+          _rowMainAxisSize(MainAxisSize.min),
+          SizedBox(height: 10),
+          Text(
+            "Row MainAxisSize.max",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _rowMainAxisSize(MainAxisSize.max),
+
+          // Columns CrossAxisAlignment
+          SizedBox(height: 10),
+          Text(
+            "Column VerticalDirection.down",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnVerticalDirection(VerticalDirection.down),
+          SizedBox(height: 10),
+          Text(
+            "Column VerticalDirection.up",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _columnVerticalDirection(VerticalDirection.up),
+        ],
+      )),
     );
   }
 }
