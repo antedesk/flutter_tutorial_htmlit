6acb51b56979e4b12e9ca33382dfca3e3761c83e
Antedesk <antedesk@gmail.com>
2019-07-28 20:12:42 +0200

    added new examples

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,35 @@ class MyPage extends StatelessWidget {
         ],
       );
 
+  Widget _setFitStack(fit) => Stack(
+        fit: fit,
+        children: [
+          Container(
+            color: Colors.greenAccent,
+            height: 100.0,
+            width: 100.0,
+          )
+        ],
+      );
+
+  Widget _setOverflowStack(overflow) => Container(
+        margin: EdgeInsets.only(top: 10),
+        color: Colors.yellow[800],
+        constraints: BoxConstraints.expand(height: 38),
+        child: Stack(
+          overflow: overflow,
+          children: <Widget>[
+            Positioned(
+              top: 10,
+              child: Text(
+                "cogito ergo sum, cogito ergo sum, cogito ergo sum, cogito ergo sum,\ncogito ergo sum, cogito ergo sum",
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            )
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +85,7 @@ class MyPage extends StatelessWidget {
 
           Container(
               margin: EdgeInsets.only(top: 10),
+              color: Colors.grey[350],
               child: Stack(
                 children: <Widget>[
                   // Max Size
@@ -84,6 +114,7 @@ class MyPage extends StatelessWidget {
           ),
           Container(
             margin: EdgeInsets.only(top: 10),
+            color: Colors.grey[350],
             child: Stack(
               alignment: Alignment.centerRight,
               children: <Widget>[
@@ -106,6 +137,111 @@ class MyPage extends StatelessWidget {
             ),
           ),
 
+          // Setting elements position using Align
+          Text(
+            "Setting elements position using Align",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.grey[350],
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  color: Colors.orange,
+                  height: 300,
+                  width: 300,
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  height: 200.0,
+                  width: 200.0,
+                ),
+                Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      color: Colors.greenAccent,
+                      height: 100.0,
+                      width: 100.0,
+                    ))
+              ],
+            ),
+          ),
+
+          // Setting elements position using Positioned
+          Text(
+            "Setting elements position using Positioned",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.grey[350],
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  color: Colors.orange,
+                  height: 300,
+                  width: 300,
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  height: 200.0,
+                  width: 200.0,
+                ),
+                Positioned(
+                    right: 50.0,
+                    top: 60.0,
+                    child: Container(
+                      color: Colors.greenAccent,
+                      height: 100.0,
+                      width: 100.0,
+                    ))
+              ],
+            ),
+          ),
+
+          // Fit Property
+          Text(
+            "fit: StackFit.loose",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+            constraints: BoxConstraints.expand(height: 160),
+            child: _setFitStack(StackFit.loose),
+          ),
+
+          Text(
+            "fit: StackFit.expand",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+            constraints: BoxConstraints.expand(height: 160),
+            child: _setFitStack(StackFit.expand),
+          ),
+
+          Text(
+            "fit: StackFit.passthrough",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Container(
+            color: Colors.black,
+            child: _setFitStack(StackFit.passthrough),
+          ),
+
+          // Overflow
+          Text(
+            "overflow: Overflow.visible",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _setOverflowStack(Overflow.visible),
+          SizedBox(height: 10),
+          Text(
+            "overflow: Overflow.clip",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          _setOverflowStack(Overflow.clip),
+
+          // final example
           SizedBox(height: 10),
           Text(
             "Example of Stack for displaying user pic",
