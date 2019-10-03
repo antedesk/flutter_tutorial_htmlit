d86256d99f000aba49e922d80fa57edb5897054b
Antedesk <antedesk@gmail.com>
2019-10-04 01:25:43 +0200

    added examples about Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -7,105 +7,346 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyDrawer(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyDrawer extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lesson 19'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Default Scaffold Body"),
+        ),
+      ),
+      drawer: MyAlwaysAvailableDrawer(),
+    );
+  }
+
+  Widget _myBasicDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Item 1'),
+            onTap: () {
+              print('Item 1');
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {
+              print('Item 2');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myDrawerWithHeaderAndDivider(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Header'),
+            decoration: BoxDecoration(
+              color: Colors.deepOrange,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('Item 1'),
+            onTap: () {
+              print('Item 1');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.view_agenda),
+            title: Text('Item 2'),
+            onTap: () {
+              print('Item 2');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.local_bar),
+            title: Text('Item 2'),
+            onTap: () {
+              print('Item 2');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.local_cafe),
+            title: Text('Item 3'),
+            onTap: () {
+              print('Item 3');
+            },
+          ),
+          Divider(),
+          ListTile(
+            trailing: Icon(Icons.close),
+            title: Text('Close'),
+            onTap: () {
+              print('Closed!');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myDrawerWithAccountHeader(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Antedesk"),
+            accountEmail: Text("myawesomeemail@example.it"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              /*Theme.of(context).platform == TargetPlatform.iOS
+                  ? Colors.blue
+                  : Colors.white,*/
+              child: Icon(Icons.person, size: 60),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('Item 1'),
+            onTap: () {
+              print('Item 1');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.view_agenda),
+            title: Text('Item 2'),
+            onTap: () {
+              print('Item 2');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.local_cafe),
+            title: Text('Item 3'),
+            onTap: () {
+              print('Item 3');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myDrawerWithAccountHeaderAndSecondIcon(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Antedesk"),
+            accountEmail: Text("myawesomeemail@example.it"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              child: Icon(Icons.person, size: 60),
+            ),
+            otherAccountsPictures: <Widget>[
+              CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 20.0),
+                  )),
+              Icon(Icons.shop, size: 30, color: Colors.white)
+            ],
+          ),
+          ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('Item 1'),
+            onTap: () {
+              print('Item 1');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.local_bar),
+            title: Text('Item 2'),
+            onTap: () {
+              print('Item 2');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.local_cafe),
+            title: Text('Item 3'),
+            onTap: () {
+              print('Item 3');
+            },
+          ),
+          Divider(),
+          ListTile(
+            trailing: Icon(Icons.close),
+            title: Text('Close'),
+            onTap: () {
+              print('Closed!');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myDrawerwithHeaderAndNavigation(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Antedesk"),
+            accountEmail: Text("myawesomeemail@example.it"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              child: Icon(Icons.person, size: 60),
+            ),
+            otherAccountsPictures: <Widget>[
+              CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 20.0),
+                  )),
+              Icon(Icons.shop, size: 30, color: Colors.white)
+            ],
+          ),
+          ListTile(
+            leading: Icon(Icons.local_bar),
+            title: Text('Item 1'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new NewPage('Item 1')));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.local_cafe),
+            title: Text('Item 2'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new NewPage('Item 2')));
+            },
+          ),
+          Divider(),
+          ListTile(
+            trailing: Icon(Icons.close),
+            title: Text('Close'),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MyAlwaysAvailableDrawer extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Antedesk"),
+              accountEmail: Text("myawesomeemail@example.it"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.grey[200],
+                child: Icon(Icons.person, size: 60),
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 20.0),
+                    )),
+                Icon(Icons.shop, size: 30, color: Colors.white)
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.local_bar),
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new NewPageV2('Item 1')));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.local_cafe),
+              title: Text('Item 2'),
+              onTap: () {
+                //Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new NewPageV2('Item 2')));
+              },
+            ),
+            Divider(),
+            ListTile(
+              trailing: Icon(Icons.close),
+              title: Text('Close'),
+              onTap: () => Navigator.pop(context),
+            ),
+          ],
+        )
+    );
   }
+}
+
+
+class NewPage extends StatelessWidget {
+  final String title;
+
+  NewPage(this.title);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: Center(child: Text("I belongs to $title"))
+    );
+  }
+}
+
+class NewPageV2 extends StatelessWidget {
+  final String title;
+
+  NewPageV2(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child: Text("I belongs to $title")),
+      drawer: MyAlwaysAvailableDrawer(),
     );
   }
 }
