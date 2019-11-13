838e3a1984a170f3cc93e15002bc527d9c784230
Antedesk <antedesk@gmail.com>
2019-11-13 01:36:04 +0100

    added basic and advanced routing examples

--- lib/main.dart
+++ lib/main.dart
@@ -9,103 +9,207 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(), //NewPageV2()
+      //initialRoute: '/thirdpage',
+      //routes: noSlashRouting(),
+      /*
+        // uncomment for the step related to unknown routes
+        onUnknownRoute: (RouteSettings setting) {
+          String unknownRoute = setting.name ;
+          return new MaterialPageRoute(
+              builder: (context) => NotFoundPage()
+          );
+        }*/
+      /*
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context)=> HomePageV2());
+            break;
+          case '/newpage':
+            return MaterialPageRoute(builder: (context)=> NewPageV2());
+            break;
+          default:
+            return MaterialPageRoute(builder: (context)=> NotFoundPage());
+        }
+      },
+      */
+    );
+  }
+
+  Map<String, WidgetBuilder> mainRouting(){
+    return {
+      // When navigating to the "/" route, build the HomePage widget.
+      '/': (context) => HomePageV2(),
+      // When navigating to the "/second" route, build the NewPage widget.
+      '/newpage': (context) => NewPageV2(),
+    };
+  }
+
+  /*
+   * this method must be used in correlation with the
+   * initialRoute: '/thirdpage',
+   */
+  Map<String, WidgetBuilder> threeRouting(){
+    return {
+      // When navigating to the "/" route, build the HomePage widget.
+      '/': (context) => HomePageV2(),
+      // When navigating to the "/second" route, build the NewPage widget.
+      '/newpage': (context) => NewPageV2(),
+      '/thirdpage': (context) => ThirdPageV2(),
+    };
+  }
+
+  /*
+   * this method must be used in correlation with the
+   * home: HomePage(),
+   */
+  Map<String, WidgetBuilder> noSlashRouting(){
+    return {
+      // When navigating to the "/" route, build the HomePage widget.
+      '/home': (context) => HomePageV2(),
+      // When navigating to the "/second" route, build the NewPage widget.
+      '/newpage': (context) => NewPageV2(),
+    };
+  }
+
+}
+
+
+/*
+ * Simple Routing Example
+ */
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the NewPage'),
+          onPressed: () {
+            Route route = MaterialPageRoute(builder: (context) => NewPage());
+            Navigator.push(context, route);
+          },
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class NewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("NewPage"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/*
+ * Named Routing Example
+ */
+
+class HomePageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the NewPage'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/newpage');
+            //Navigator.pushNamed(context, '/newp'); // uncomment to test the unknown routing
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class NewPageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NewPage"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ThirdPage"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/');
+          },
+          child: Text('Go to HomePage!'),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+ * UnknownRoute management.
+ * To use in combo with
+ *
+  onUnknownRoute: (RouteSettings setting) {
+    String unknownRoute = setting.name ;
+    return new MaterialPageRoute(
+        builder: (context) => NotFoundPage()
+    );
+  }
+ */
+
+class NotFoundPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PageNotFound"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back to HomePage!'),
+        ),
+      ),
     );
   }
 }
