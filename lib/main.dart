3e2de9c7fa0451ceae78a8e94bf6a6aab2d5ebe0
Antonio Tedeschi <antonio.tedeschi@immobiliare.it>
2019-11-15 18:49:25 +0100

    added samples for data sharing with routing

--- lib/main.dart
+++ lib/main.dart
@@ -20,92 +20,171 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: HomePage(), // first example
+      /* //uncomment to use the real example
+      home: ArticleListPage(
+        articles: List.generate(
+          20,
+              (i) =>
+              Article(
+                'Article $i',
+                'This is a simple text for Article $i',
+              ),
+        ),
+      ),*/
+      routes: <String, WidgetBuilder>{
+        '/': (context) => HomePageV2(),
+        '/article_details': (context) => ArticleDetailPageV2(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+/*
+ * This class should be put in a proper dart file for managing the entities of
+ * our app. For this simple tutorial is ok to place it in the main.dart file
+ */
+class Article {
   final String title;
+  final String description;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Article(this.title, this.description);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/*
+ * Simple Routing
+ */
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Article article =
+                new Article('Article', 'This is a simple text for Article');
+            Route route = MaterialPageRoute(
+              builder: (context) => ArticleDetailPage(article: article),
+            );
+            Navigator.push(context, route);
+          },
+          child: Text('Open ArticleDetailPage'),
+        ),
+      ),
+    );
   }
+}
+
+
+class ArticleDetailPage extends StatelessWidget {
+  final Article article;
+
+  ArticleDetailPage({Key key, this.article}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(article.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(article.description),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+/*
+* Real example
+*/
+
+class ArticleListPage extends StatelessWidget {
+  final List<Article> articles;
+
+  ArticleListPage({Key key, @required this.articles}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: articles.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(articles[index].title),
+            // When a user taps the ListTile, navigate to the ArticleDetailPage.
+            // Notice that you're not only creating a ArticleDetailPage, you're
+            // also passing the current article through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ArticleDetailPage(article: articles[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+/*
+ * Named Routing
+ */
+
+class HomePageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Article article = new Article('Article', 'This is a simple text for Article for Named Routing');
+            Navigator.pushNamed(context, '/article_details', arguments: article);
+          },
+          child: Text('Open ArticleDetailPage'),
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleDetailPageV2 extends StatefulWidget {
+  @override
+  _ArticleDetailPageV2State createState() => _ArticleDetailPageV2State();
+}
+
+class _ArticleDetailPageV2State extends State<ArticleDetailPageV2> {
+  Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    RouteSettings settings = ModalRoute.of(context).settings;
+    article = settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(article.description),
+      ),
+    );
+  }
+}
+
