5d44d971f57a964d755cfab4f94ca09ab38841a1
Antonio Tedeschi <antonio.tedeschi@immobiliare.it>
2020-05-15 00:56:14 +0200

    simple example about how to use gmaps

--- lib/screens/screen1/screen1.dart
+++ lib/screens/screen1/screen1.dart
@@ -1,14 +1,64 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_tutorial_htmlit/screens/screen1/components/body.dart';
+import 'dart:async';
 
-class Screen1 extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Screen1 extends StatefulWidget {
+  @override
+  State<Screen1> createState() => Screen1State();
+}
+
+class Screen1State extends State<Screen1> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final LatLng colosseoLatLng = LatLng(41.890251, 12.492373);
+
+  static final CameraPosition colosseo = CameraPosition(
+    target: LatLng(41.890251, 12.492373),
+    zoom: 16.5,
+  );
+
+  static final Marker colosseoMarker = Marker(
+    // This marker id can be anything that uniquely identifies each marker.
+    markerId: MarkerId(colosseoLatLng.toString()),
+    //_lastMapPosition is any coordinate which should be your default
+    //position when map opens up
+    position: colosseoLatLng,
+    infoWindow: InfoWindow(
+      title: 'Colosseo',
+      snippet: 'Roma, Italy',
+    ),
+    icon: BitmapDescriptor.defaultMarker,
+  );
+
+  static final Polyline foriImperiali = Polyline(
+    polylineId: PolylineId(colosseoLatLng.toString()),
+    visible: true,
+    width: 7,
+    color: Colors.green,
+    points: [
+      LatLng(41.891456, 12.490189),
+      LatLng(41.891436, 12.490261),
+      LatLng(41.891348, 12.490476),
+      LatLng(41.891300, 12.490733),
+      LatLng(41.891260, 12.490911)
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Screen1'),
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: colosseo,
+        polylines: <Polyline>{foriImperiali},
+        markers: <Marker>{colosseoMarker},
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
-      body: Body(),
     );
   }
 }
